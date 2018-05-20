e7e12fbe8edf9b640b73e15c6ad77cfa037d9dce
Allek Mott <allekmott@gmail.com>
2018-05-20 18:28:51 -0400

    Implement external IP lookup, core loop logic

--- wancheck.py
+++ wancheck.py
@@ -4,6 +4,7 @@ Simple script to periodically check to see if a machine's external IP has
 changed
 """
 
+import sys
 import time
 
 import dns.message
@@ -13,12 +14,16 @@ import dns.rdataclass
 import dns.query
 
 VERSION = str("0.0.1")
-_CHECK_INTERVAL = 1
 
-def check_ip():
-	print("check ip")
+_CHECK_INTERVAL = 10
+
+_LOOKUP_DOMAIN = "myip.opendns.com"
+_RESOLVER_DOMAIN = "resolver1.opendns.com"
+
+def ip_lookup(domain, nameserver_ip="8.8.8.8"):
+	"""Run DNS lookup against domain, return first result or None"""
 	query_message = dns.message.make_query(
-			qname="www.google.com",
+			qname=domain,
 			rdtype=dns.rdatatype.A,
 			rdclass=dns.rdataclass.IN,
 
@@ -33,7 +38,7 @@ def check_ip():
 	query_response = dns.query.udp(
 			q=query_message,
 			af=None,
-			where="8.8.8.8",
+			where=nameserver_ip,
 			port=53,
 			timeout=2,
 
@@ -45,19 +50,63 @@ def check_ip():
 			)
 
 	query_answers = query_response.answer
+
+	ip_address = None
 	for answer in query_answers:
-		for address in answer.items:
-			print("%i %s" % (address.rdtype, address.address))
+		ipv4_records = \
+				filter(lambda r: r.rdtype == dns.rdatatype.A, answer.items)
 
-	return query_response
+		if len(ipv4_records):
+			ip_address = ipv4_records[0].address
+			break
 
+	return ip_address
+
+def alert(ip):
+	pass
 
 def main():
-	print("wancheck v%s" % (VERSION))
+	print("wancheck v%s\n" % (VERSION))
 
-	# while True:
-	check_ip()
-	# 	time.sleep(_CHECK_INTERVAL)
+	try:
+		resolver_ip = ip_lookup(_RESOLVER_DOMAIN)
+	except Exception as e:
+		print("Unable to look up resolver IP: %s" % (e))
+		return
+
+	print("using resolver '%s' (%s)" % (_RESOLVER_DOMAIN, resolver_ip))
+	print("using lookup domian '%s'" % (_LOOKUP_DOMAIN))
+
+	ip_lastest = None
+	while True:
+		try:
+			ip = ip_lookup(_LOOKUP_DOMAIN, resolver_ip)
+		except Exception as e:
+			if hasattr(e, "errno") and type(e.errno) is int:
+				sys.stderr.write("E%i" % (e.errno))
+			else:
+				sys.stderr.write("E")
+
+			sys.stderr.flush()
+
+			time.sleep(_CHECK_INTERVAL)
+			continue
+
+		if ip is None:
+			sys.stdout.write("x")
+			sys.stdout.flush()
+
+		if ip_lastest is None:
+			print("\nstarting ip: %s" % (ip))
+			ip_lastest = ip
+		elif ip != ip_lastest:
+			print("\nNEW IP: %s" % (ip))
+			alert(ip)
+		else:
+			sys.stdout.write(".")
+			sys.stdout.flush()
+
+		time.sleep(_CHECK_INTERVAL)
 
 if __name__ == "__main__":
 	main()
